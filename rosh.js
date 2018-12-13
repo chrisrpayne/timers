e8ca4d72d01925c0558ed3324d6b1f81e9675a7a
Chris R. Payne <06cpayne@gmail.com>
2018-12-13 18:00:22 -0500

    rosh.js

--- rosh.js
+++ rosh.js
@@ -22,7 +22,8 @@ addSound(1, 180, 0, roshSounds[3], '2 minutes left!');
 addSound(1, 240, 0, roshSounds[0], '1 minute left!');
 addSound(1, 241, 0, roshSounds[1], '1 minute left!');
 addSound(1, 242, 0, roshSounds[2], '1 minute left!');
-addSound(1, 270, 0, roshSounds[4], 'Aegis GONE');
+addSound(1, 270, 0, roshSounds[4], 'Aegis expires soon!');
+addSound(1, 300, 0, roshSounds[4], 'Aegis expired!');
 addSound(1, 480, 0, roshSounds[3], 'Rosh could be back up!');
 addSound(1, 660, 0, roshSounds[5], 'Rosh is definitely back up!');
 
